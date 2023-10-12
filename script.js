ad7abb17933ce3487004860cc4ebcb57828f88f6
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-10-12 15:17:58 -0400

    inserted code to commit tasks to local storage, added code to clear all tasks from local storage

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let entry = document.getElementById("input");
 let taskList = document.getElementById("taskList");
+let myArray = [];
+
+if(localStorage.getItem("taskEntry") === null){
+    myArray = [];
+} else {
+    myArray = JSON.parse(localStorage.getItem("taskEntry"));
+}
+
+function removeItem(){
+    this.parentNode.remove();
+}
 
 function addTask(){
     let li = document.createElement('li');
     li.innerHTML = entry.value;
-    entry.value = "";
+    
 
     taskList.appendChild(li);
 
@@ -12,4 +23,25 @@ function addTask(){
     deleteBtn.innerText = "Delete";
     deleteBtn.setAttribute('id','dB');
     li.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", removeItem)
+
+
+
+    let myObj = {
+        item: entry.value,
+        id: Math.floor(Math.random()*100)
+    }
+
+    myArray.push(myObj);
+
+    entry.value = "";
+
+    let myObjString = JSON.stringify(myArray);
+    localStorage.setItem("taskEntry", myObjString);
+}
+
+function clearBtn(){
+    localStorage.removeItem("taskEntry");
+    taskList.innerHTML = "";
 }
